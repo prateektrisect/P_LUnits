84e7f0bb90ddde49aff3a25c32f6d9b0ec16f8cd
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 15:18:10 +0530

    Update main.py

--- P1/T3/main.py
+++ P1/T3/main.py
@@ -1,25 +1,4 @@
-import java.util.Scanner;
-public class Main{
-
-  void solution(int n) {
-
-    // Here testing system will provide
-    // values to variable n, you don't 
-    // need to assign any values to them
-    
-    // Write your code here
-    // Complete the sample code provided below
-    for(int i = 1; i <= n; i++) {            
-        System.out.println();  //print i here
-    }
-  }
-
-  //Test code
-  //Don't change anything below this line
-  public static void main(String[] args){
-    Main test = new Main();
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    test.solution(n);
-  }
-}
+if __name__ == '__main__':
+  #Use for loop from 1 to n
+  #write your code here
+  
