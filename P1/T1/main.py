801f27b6951a779b2fd337b22216f4ddfb463a13
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-12 15:01:19 +0530

    Update main.py

--- P1/T1/main.py
+++ P1/T1/main.py
@@ -1,7 +1,4 @@
-# if __name__ == "__main__":
-#    // no need to take any input
-#    // copy given code and paste here
-a = 10
-b = 20
-sum = a + b
-print(sum)
+if __name__ == "__main__":
+  #no need to take any input
+  #copy given code and paste here
+
