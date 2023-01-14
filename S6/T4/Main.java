07bdd121cc07c6984c6b63ad8bc2c864875c9e21
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-14 09:35:50 -0800

    Update Main.java

--- S6/T4/Main.java
+++ S6/T4/Main.java
@@ -23,13 +23,13 @@ public class Main{
         array[i]=sc.nextInt();  
     } 
     
-     Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();
+     Scanner sc1 = new Scanner(System.in);
+    int x = sc1.nextInt();
     int[] array1 = new int[x]; 
     for(int i=0; i<x; i++)  
     {  
          
-        array1[i]=sc.nextInt();  
+        array1[i]=sc1.nextInt();  
     } 
     test.solution(array,array1);
   }
