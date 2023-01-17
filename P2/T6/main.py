5b3314348cc7893781771529a9c1d370cabca425
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 14:46:38 +0530

    Update main.py

--- P2/T6/main.py
+++ P2/T6/main.py
@@ -3,9 +3,13 @@
 if __name__ == "__main__":
   n = int(input())
   
-  
+# Write your code here 
 # Remember the output should match exactly
-# Write your code here
+# Here testing system will provide
+# values to variable n, you don't 
+# need to assign any values to them
+# Complete the sample code provided below
     
-
+for i in range(1, n+1):
+  print()           # print pattern here in format given
   
