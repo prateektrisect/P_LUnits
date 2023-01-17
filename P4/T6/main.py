9d7ab268e1791d7f25a514af7c1882fb0296c674
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:12:50 +0530

    Update main.py

--- P4/T6/main.py
+++ P4/T6/main.py
@@ -1,11 +1,11 @@
 # Test code
 # Don't change anything in this block
 if __name__ == "__main__":
-  n1 = int(input())
-  n2 = int(input())
+  x = int(input())
+  y = int(input())
   
  
 # Write your code here 
-# values to variable n1 and n2, you don't 
+# values to variable x and y, you don't 
 # need to assign any values to them
  
