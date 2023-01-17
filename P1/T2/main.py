c5843fd67d9c30bac0ef1c472678665edd4d3d35
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 12:50:55 +0530

    Update main.py

--- P1/T2/main.py
+++ P1/T2/main.py
@@ -1,3 +1,11 @@
-if __name__ == '__main__':
-  # write your code here
-print(sum)
+# no need to assign any values of a and b.
+# write your code here
+sum =             # should be int sum = a + b;
+print()           # print sum
+
+
+# Test code
+# Don't change anything below this line
+if __name__ == __main__:
+  a = int(input())
+  b = int(input())
