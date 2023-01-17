091afdb57230d52af88bf850e5402e66fb093adb
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:25:46 +0530

    Update main.py

--- Q1/T2/main.py
+++ Q1/T2/main.py
@@ -1,3 +1,9 @@
-if __name__ == '__main__'
-  # Use for loop  
-  # Write your code here
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  st = input()
+  
+# Write your code here 
+# values to String variable st, you don't 
+# need to assign any values to them
+#   Print total number of uppercase and lowercase characters. 
