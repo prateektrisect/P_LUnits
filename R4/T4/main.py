8cf8432ec5d04442ae4546ef158c3e029f546614
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 17:01:25 +0530

    Update main.py

--- R4/T4/main.py
+++ R4/T4/main.py
@@ -1,4 +1,9 @@
-if __name__ == '__main__'
-  # Use for loop 
-  # Write your code here
-
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  st = input()
+  
+# Write your code here 
+# values to variable st, you don't 
+# need to assign any values to string st
+ 
