158f9ea76ab0006d9c519882688db98a76b79a08
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:42:42 +0530

    Update main.py

--- Q4/T3/main.py
+++ Q4/T3/main.py
@@ -1,3 +1,9 @@
-if __name__ == '__main__'
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  st = input()
   
-  # Write your code here
+# Write your code here 
+# Remember the output should match exactly 
+# Given string name st, you don't need to assign any value
+# Create a new string with all the lowercase characters
