5f83de887a5c4861f644dec7756d11f8a68f75f8
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:11:58 +0530

    Update main.py

--- P4/T6/main.py
+++ P4/T6/main.py
@@ -1,3 +1,11 @@
-if __name__ == '__main__'
-  # Use for loop   
-  # Write your code here
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n1 = int(input())
+  n2 = int(input())
+  
+ 
+# Write your code here 
+# values to variable n1 and n2, you don't 
+# need to assign any values to them
+ 
