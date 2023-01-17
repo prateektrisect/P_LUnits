547da7db420f8a5a96b2526ed7e66b27191ff007
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 16:42:39 +0530

    Update main.py

--- R2/T1/main.py
+++ R2/T1/main.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__'
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  # For list of integers
+  lst1 = []  
+  lst1 = [int(item) for item in input().split()]
+
   
-  # Write your code here
+# Write your code here 
+# values to list name lst1, you don't 
+# need to assign any values to them
+
