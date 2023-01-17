dcd140d7f983aa9360568de8a4221e7eacc29bf0
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:23:38 +0530

    Update main.py

--- Q1/T1/main.py
+++ Q1/T1/main.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__'
-  # Use for loop    
-  # Write your code here
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  st = input()
+  
+# Write your code here 
+# Remember the output should match exactly
+# value to string variable st, you don't need to assign 
+# apply if to check character is z then count +1
+      
+
+    
