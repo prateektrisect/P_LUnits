26dcc1615f463149d69b791c1cdc022a7e6ce8ea
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:01:13 +0530

    Update main.py

--- P3/T1/main.py
+++ P3/T1/main.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
   n = int(input())
   
 # Write your code here 
-# Remember the output should match exactly
-# Here testing system will provide
 # values to variable n, you don't 
 # need to assign any values to them
 # Use if else 
