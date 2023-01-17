0b38fe70184ba01d9550c1a2e14f19731bb5c190
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:09:33 +0530

    Update and rename Main.java to main.py

---
+++ P4/T4/main.py
@@ -0,0 +1,11 @@
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
+  
+# Write your code here 
+# values to variable n, you don't 
+# need to assign any values to them
+#  Use for loop in reverse order and use accumulator to achive pattern
+ 
