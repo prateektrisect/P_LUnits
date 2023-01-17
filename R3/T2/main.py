37bfdd7b5a5cba5c4882b8858ab4cd310ec013e6
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 16:59:25 +0530

    Update main.py

--- R3/T2/main.py
+++ R3/T2/main.py
@@ -1,3 +1,9 @@
-if __name__ == '__main__'
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  st = input()
   
-  # Write your code here
+# Write your code here 
+# values to variable st, you don't 
+# need to assign any values to string st
+ 
