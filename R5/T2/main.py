f68ab52b1fd85b6a4ea4252f4c7a7c24b41a0009
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 17:06:31 +0530

    Update main.py

--- R5/T2/main.py
+++ R5/T2/main.py
@@ -1,3 +1,15 @@
-if __name__ == '__main__'
-  # Use for loop
-  # Write your code here
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  lst1 = []  
+  lst1 = [int(item) for item in input("Enter the list items : ").split()]
+
+  lst2 = []  
+  lst2 = [item for item in input("Enter the list items : ").split()]
+
+  
+# Write your code here 
+# values to list lst1 and lst2, you don't 
+# need to assign any values to them
+ 
