add45d51dd7f689d33cb9f365ea3c34a005d9b06
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 17:16:52 +0530

    Update main.py

--- R2/T2/main.py
+++ R2/T2/main.py
@@ -3,9 +3,10 @@
 if __name__ == "__main__":
   # For list of integers
   lst1 = []  
-  lst1 = [int(item) for item in input().split()]
+  n = int(input())
+  lst1 = [int(item) f
+  or item in input().split()]
 
-  
 # Write your code here 
 # values to list name lst1, you don't 
 # need to assign any values to them
