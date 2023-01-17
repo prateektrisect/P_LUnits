594a76c359f7320186760f30deea3f45bde0eead
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 13:13:43 +0530

    Update main.py

--- P1/T3/main.py
+++ P1/T3/main.py
@@ -1,11 +1,11 @@
 # Test code
-# Don't change anything below this line
-if __name__ == '__main__':
+# Don't change anything in this block
+if __name__ == "__main__":
   n = int(input())
 
 # Complete the sample code provided below
-    for i in range(1, n+1):
-      # write your code here
+for i in range(1, n+1):
+  # write your code here
   
 
 
