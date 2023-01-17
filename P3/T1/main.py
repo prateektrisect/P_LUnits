e66ba39b190372767f676250e5b7b13bbf175cf7
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:00:48 +0530

    Update main.py

--- P3/T1/main.py
+++ P3/T1/main.py
@@ -1,4 +1,11 @@
-if __name__ == '__main__'
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
   
-  # Write your code here
-  
+# Write your code here 
+# Remember the output should match exactly
+# Here testing system will provide
+# values to variable n, you don't 
+# need to assign any values to them
+# Use if else 
