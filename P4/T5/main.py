aa4db426c15be614786a7a3348cfc2fb0cbeb740
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 15:10:34 +0530

    Update main.py

--- P4/T5/main.py
+++ P4/T5/main.py
@@ -1,4 +1,10 @@
-if __name__ == '__main__'
-  # Use for loop 
-  # Write your code here
-
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
+  
+# Write your code here 
+# values to variable n, you don't 
+# need to assign any values to them
+# Use 2 for loop 
+# 1st for ascending order and 2nd for descending order
