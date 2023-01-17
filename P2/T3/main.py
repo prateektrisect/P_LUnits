a962ef1923ff04791e211e42f5f413db19f608cf
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-17 14:52:13 +0530

    Update main.py

--- P2/T3/main.py
+++ P2/T3/main.py
@@ -2,10 +2,13 @@
 # Don't change anything in this block
 if __name__ == "__main__":
   n = int(input())
-  
-  
-# Remember the output should match exactly
-# Write your code here
-    
 
-  
+# Remember the output should match exactly
+# Misplaced character, extra spaces, 
+# missing character, letters not in exact case will
+# will fail the test  
+
+# no need to assign value of n
+# Write your code here
+
+ 
