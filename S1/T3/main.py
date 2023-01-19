d4209951a2bfea663a242632078ad549ab9ec89d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 16:16:06 +0530

    Update main.py

--- S1/T3/main.py
+++ S1/T3/main.py
@@ -3,10 +3,8 @@
 def square(n):
   squareN = n*n
   print(squareN)
- 
-# given list lst1
-if __name__ == "__main__":
-  n = int(input())
-lst1 = [1, 7, 9, 12]
 
+  
 # Write code here
+# given list lst1
+lst1 = [1, 7, 9, 12]
