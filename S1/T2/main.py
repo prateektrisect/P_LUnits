ced69b3a4d2f6cd4d1c91d66c085207d34752080
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 16:15:08 +0530

    Update main.py

--- S1/T2/main.py
+++ S1/T2/main.py
@@ -4,12 +4,7 @@ def square(n):
   squareN = n*n
   print(squareN)
 
-
-# Test code
-# Don't change anything in this block
-if __intit__ == "main":
-  n = int(input())
-
   
 # Write your code here
-# print square of 11, 17 and 23 
+# print square of 11, 17 and 23
+# using square() function
