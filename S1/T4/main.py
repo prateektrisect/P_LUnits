9e051e2eee23f3ce443e99f3db95779755a40334
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 15:40:37 +0530

    Update main.py

--- S1/T4/main.py
+++ S1/T4/main.py
@@ -1,14 +1,19 @@
 
+# Given square() function
 def square(n):
   squareN = n*n
   print(square)
 
-# Write your code here 
-# values to variable n, you don't 
-# need to assign any value to n
+# Write your code here
+# Create cube() function 
+# you don't need to assign any value to n
+
+
+
  
 # Test code
 # Don't change anything in this block
 if __name__ == "__main__":
   n = int(input())
   
+# Write your code here and call the function
