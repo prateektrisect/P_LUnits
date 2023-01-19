1121a1dd858e8d78c5a30667536421cc84c4860d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 14:44:34 +0530

    Update main.py

--- S1/T2/main.py
+++ S1/T2/main.py
@@ -1,11 +1,9 @@
 # Test code
 # Don't change anything in this block
-if __name__ == "__main__":
-  n = int(input())
+def square(n):
+  squareN = n*n
+  print(squareN)
+  
   
 # Write your code here 
-# values to variable n, you don't 
-# need to assign any value to n
 
-
-#
