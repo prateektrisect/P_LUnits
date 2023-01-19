3c1dc5e87e74614724dac6120ad4d7a689952713
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 14:40:53 +0530

    Create main.py

---
+++ S1/T2/main.py
@@ -0,0 +1,11 @@
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
+  
+# Write your code here 
+# values to variable n, you don't 
+# need to assign any value to n
+
+
+#
