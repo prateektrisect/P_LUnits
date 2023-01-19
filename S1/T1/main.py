60cd1054ea3d29f8ccbd9834c76fa755171fca38
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 15:29:38 +0530

    Update main.py

--- S1/T1/main.py
+++ S1/T1/main.py
@@ -1,9 +1,15 @@
+# values to variable n, you don't 
+# need to assign any value to n
+# Copy square function here 
+ 
+
+  
+  
+
 # Test code
 # Don't change anything in this block
 if __name__ == "__main__":
   n = int(input())
   
-# Copy square function here and run it
-# values to variable n, you don't 
-# need to assign any value to n
- 
+#call your square() function here
+# write square(n)
