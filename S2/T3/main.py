ca7c8487782f746939d686e3367f36fe06af3a80
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 16:54:42 +0530

    Update and rename Main.java to main.py

---
+++ S2/T3/main.py
@@ -0,0 +1,11 @@
+# Write your code here 
+# to create the function
+
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
+  
+# Write your code here 
+# and Call the function
