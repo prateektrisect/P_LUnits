d5e15e68939e38b12bd0f4bbc9816029e5853034
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 14:34:50 +0530

    Update and rename Main.java to main.py

---
+++ S1/T1/main.py
@@ -0,0 +1,9 @@
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  n = int(input())
+  
+# Write square function here 
+# values to variable n, you don't 
+# need to assign any value to n
+ 
