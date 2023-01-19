a2e5c9230ce4948eb8851c7c64cb312f5c6ac8d1
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-19 16:50:35 +0530

    Update and rename Main.java to main.py

---
+++ S2/T2/main.py
@@ -0,0 +1,18 @@
+# Write your code here
+# Create function 
+# you don't need to assign any value to the list
+
+
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  # For list of integers
+  lst1 = []  
+  n = int(input())
+  lst1 = [int(item) for item in input().split()]
+  
+  
+  
+#call your function here
+# passing list lst1 as input like function_name(lst1)
