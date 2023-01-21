c715eb2e49a7c9bb7d75073f82145b5e2b98da7b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-21 10:10:01 +0530

    Update and rename Main.java to main.py

---
+++ S4/T5/main.py
@@ -0,0 +1,24 @@
+# Given function
+def grades(marks):
+  if(marks>=90):
+    print("Grade A")
+  elif(marks>=75):
+    print("Grade B")
+  elif((marks>=40):
+    print("Grade C")
+  else:
+    print("Fail")
+  
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  # For list of integers
+  lst1 = []  
+  n = int(input())
+  lst1 = [int(item) for item in input().split()]
+  
+  
+  
+# Write code here 
+# and call your function 
