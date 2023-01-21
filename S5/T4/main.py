f62d50dda2cdca4cbfeacda1ab24889d47713af9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-01-21 10:49:54 +0530

    Update and rename Main.java to main.py

---
+++ S5/T4/main.py
@@ -0,0 +1,20 @@
+# Write code here
+# to create a function
+
+
+# Test code
+# Don't change anything in this block
+if __name__ == "__main__":
+  # For list of integers
+  lst1 = []  
+  n = int(input())
+  lst1 = [int(item) for item in input().split()]
+  
+  lst2 = []  
+  m = int(input())
+  lst2 = [int(item) for item in input().split()]
+  
+  
+  
+# Write code here 
+# and call your function 
