19aa7d93a55519992baf49eef5c78a537c2872b2
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-22 09:32:36 +0530

    Update main.py

--- Q5/T1/main.py
+++ Q5/T1/main.py
@@ -1,10 +1,16 @@
 # Test code
 # Don't change anything in this block
   # For list of integers
-  lst1 = []  
-  n = int(input())
-  lst1 = [int(item) for item in input().split()]
+# lst1 = []  
+# n = int(input())
+# lst1 = [int(item) for item in input().split()]
 
+n=int(input())
+ar_list=[]
+new=[]
+for i in range(0,n):
+    ar_list.append(int(input()))
+len1=len(ar_list)
   
 # Write your code here 
 # values to list name lst1, you don't 
