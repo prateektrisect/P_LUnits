556d997ef90ed68d0ec198edd68bd1c2b408acc7
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-23 13:28:20 +0530

    Update main.py

--- Q5/T2/main.py
+++ Q5/T2/main.py
@@ -1,12 +1,18 @@
-# Test code
-# Don't change anything in this block
-  # For list of integers
-  lst1 = []  
-  n = int(input())
-  lst1 = [int(item) for item in input().split()]
-
-  
-# Write your code here 
-# values to list name lst1, you don't 
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
 # need to assign any values to them
 
+def solution(in_list):
+  new_list = []
+
+
+  return new_list
+
+
+
+# Test code
+# Don't change anything in this block
+n = int(input())
+in_list = [int(item) for item in input().split()]
+out_list = solution(in_list)
+print(out_list)
