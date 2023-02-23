3ef97d78ae9d0c735541e12a4606a64e6d5f7d8a
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-23 13:44:31 +0530

    Update main.py

--- P1/T1/main.py
+++ P1/T1/main.py
@@ -1,3 +1,18 @@
-#no need to take any input
-#copy given code and paste here
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
 
+def solution(in_list):
+  new_list = []
+
+
+  return new_list
+
+
+
+# Test code
+# Don't change anything in this block
+n = int(input())
+in_list = [int(item) for item in input().split()]
+out_list = solution(in_list)
+print(out_list)
