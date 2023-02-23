260b98af600f1d77526ca571cbd9bb7507443936
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-23 13:23:35 +0530

    Update main.py

--- Q2/T2/main.py
+++ Q2/T2/main.py
@@ -1,7 +1,16 @@
+# Write your code inside solution function given below 
+# values to string is already given, you don't 
+# need to assign any values to them
+
+def solution(in_list):
+  st = input() # string name is st
+
+
+  return st
+
+
+
 # Test code
 # Don't change anything in this block
-  st = input()
-  
-# Write your code here 
-# Remember the output should match exactly 
-# Given string name st, you don't need to assign any value
+st = input()
+print(st)
