ef6e5c8920ed6005350138a53edea68829694399
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 10:16:27 +0530

    Update main.py

--- R4/T2/main.py
+++ R4/T2/main.py
@@ -1,8 +1,16 @@
+# Write your code inside solution function given below 
+# values to string name st is already given, you don't 
+# need to assign any values to them
+
+def solution(st):
+  
+
+  print()     # print the final result
+
+ 
 # Test code
 # Don't change anything in this block
-  st = input()
-  
-# Write your code here 
-# values to variable st, you don't 
-# need to assign any values to string st
+st = input()
+solution(st)
+
  
