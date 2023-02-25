6d0f8cc3854f5827e4390c06fd0ca784b3a891d0
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 10:21:55 +0530

    Update main.py

--- R5/T2/main.py
+++ R5/T2/main.py
@@ -1,16 +1,19 @@
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
+
+def solution(in_list1, in_list2):
+  # write your code here
+
 
 # Test code
 # Don't change anything in this block
-  lst1 = []  
-  n1 = int(input())
-  lst1 = [int(item) for item in input().split()]
+lst1 = []  
+n1 = int(input())
+lst1 = [int(item) for item in input().split()]
 
-  lst2 = []  
-  n2 = int(input())
-  lst2 = [item for item in input().split()]
+lst2 = []  
+n2 = int(input())
+lst2 = [item for item in input().split()]
+solution(lst1, lst2)
 
-  
-# Write your code here 
-# values to list lst1 and lst2, you don't 
-# need to assign any values to them
- 
