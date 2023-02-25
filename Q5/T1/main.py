a84c6e3f80a1a857c4e158072bc897f9cc0d95d8
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 09:52:58 +0530

    Update main.py

--- Q5/T1/main.py
+++ Q5/T1/main.py
@@ -1,20 +1,16 @@
-
 # Write your code inside solution function given below 
 # values to list name in_list is already given, you don't 
 # need to assign any values to them
 
 def solution(in_list):
   new_list = []
-
-
-  print() # print your list here
+ 
+  print() # print final result here
 
 
 
 # Test code
 # Don't change anything in this block
 n = int(input())
-in_list = []
-for i in range(0, len(n)):
-  in_list.append(input())
+in_list = [int(item) for item in input().split()]
 solution(in_list)
