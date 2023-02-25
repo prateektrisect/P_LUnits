691e2b6fec3490e9f2eb5f489ef925c90cb7a60c
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 09:40:07 +0530

    Update main.py

--- Q2/T1/main.py
+++ Q2/T1/main.py
@@ -1,7 +1,14 @@
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
+
+def solution(st):
+  
+
+  print()     # print the final result
+
+ 
 # Test code
 # Don't change anything in this block
-  st = input()
-  
-# Write your code here 
-# Remember the output should match exactly 
-# count of character 'x' and 'y' in given string name st
+st = input()
+solution(st)
