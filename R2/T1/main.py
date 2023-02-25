3b681451a2b6641d7601ca431f8a78c1fbbb5b4e
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 10:01:14 +0530

    Update main.py

--- R2/T1/main.py
+++ R2/T1/main.py
@@ -1,12 +1,17 @@
-# Test code
-# Don't change anything in this block
-  # For list of integers
-  lst1 = []  
-  n = int(input())
-  lst1 = [int(item) for item in input().split()]
 
-  
-# Write your code here 
-# values to list name lst1, you don't 
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
 # need to assign any values to them
 
+def solution(in_list):
+  # write your code here
+ 
+  print() # print final result here
+
+
+
+# Test code
+# Don't change anything in this block
+n = int(input())
+in_list = [int(item) for item in input().split()]
+solution(in_list)
