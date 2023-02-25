94c58a5d3a175473246b6cef6b3bdf8a94f2f766
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 09:42:58 +0530

    Update main.py

--- Q2/T2/main.py
+++ Q2/T2/main.py
@@ -1,16 +1,14 @@
 # Write your code inside solution function given below 
-# values to string is already given, you don't 
+# values to list name in_list is already given, you don't 
 # need to assign any values to them
 
-def solution(in_list):
-  st = input() # string name is st
+def solution(st):
+  
 
+  print()     # print the final result
 
-  return st
-
-
-
+ 
 # Test code
 # Don't change anything in this block
 st = input()
-print(st)
+solution(st)
