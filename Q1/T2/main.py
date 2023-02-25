67e29084e92bd0670efefe51bbc8a67bfb0cb576
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 09:39:16 +0530

    Update main.py

--- Q1/T2/main.py
+++ Q1/T2/main.py
@@ -1,8 +1,15 @@
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
+
+def solution(st):
+  
+
+  print()     # print the final result
+
+ 
 # Test code
 # Don't change anything in this block
-  st = input()
-  
-# Write your code here 
-# values to String variable st, you don't 
-# need to assign any values to them
-#   Print total number of uppercase and lowercase characters. 
+st = input()
+solution(st)
+
