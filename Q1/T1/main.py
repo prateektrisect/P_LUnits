7c82aa4a7becc2942d78b063219e485f363c5643
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-25 09:37:30 +0530

    Update main.py

--- Q1/T1/main.py
+++ Q1/T1/main.py
@@ -1,11 +1,14 @@
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
+
+def solution(st):
+  
+
+  print()     # print the final result
+
+ 
 # Test code
 # Don't change anything in this block
-  st = input()
-  
-# Write your code here 
-# Remember the output should match exactly
-# value to string variable st, you don't need to assign 
-
-      
-
-    
+st = input()
+solution(st)
