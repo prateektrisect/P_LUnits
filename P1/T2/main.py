5da5cc27debe327b8479e784489be413a10e14a9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 09:51:29 +0530

    Update main.py

--- P1/T2/main.py
+++ P1/T2/main.py
@@ -1,11 +1,15 @@
+# Write your code inside solution function given below 
+# values to string name st is already given, you don't 
+# need to assign any values to them
+
+def solution(n1, n2):
+  
+
+  print()     # print the final result
+
+ 
 # Test code
 # Don't change anything in this block
-a = int(input())
-b = int(input())
-
-  
-# no need to assign any values of a and b.
-# write your code here
-sum =             # should be int sum = a + b;
-print()           # print sum
-
+num1 = int(input())
+num2 = int(input())
+solution(num1, num2)
