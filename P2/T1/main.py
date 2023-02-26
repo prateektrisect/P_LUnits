69f5a85f0169507af926b7dfbb6e13a9a9afb6e5
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 10:10:58 +0530

    Update main.py

--- P2/T1/main.py
+++ P2/T1/main.py
@@ -1,10 +1,12 @@
+# Write your code inside solution function given below 
+# you don't need to assign any values to them
+
+def solution():
+  # write your code here
+  
+
+ 
+ 
 # Test code
 # Don't change anything in this block
-  n = int(input())
-  
-  
-# Remember the output should match exactly
-# Write your code here
-    
-
-  
+solution()
