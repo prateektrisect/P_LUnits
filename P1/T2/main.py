b5728bd23c47a6e9b98131224ed06dd3a0e6d4e9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 09:56:04 +0530

    Update main.py

--- P1/T2/main.py
+++ P1/T2/main.py
@@ -1,5 +1,5 @@
 # Write your code inside solution function given below 
-# values to string name st is already given, you don't 
+# values to n1 and n2 is already given, you don't 
 # need to assign any values to them
 
 def solution(n1, n2):
