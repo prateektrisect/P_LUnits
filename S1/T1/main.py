80e68740cd90e8ecc1185de33569e47afaa7171e
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 16:27:04 +0530

    Update main.py

--- S1/T1/main.py
+++ S1/T1/main.py
@@ -1,14 +1,5 @@
-# values to variable n, you don't 
-# need to assign any value to n
-# Copy square function here 
- 
+# Copy the given function here
+# and Hit Run button
 
-  
-  
 
-# Test code
-# Don't change anything in this block
-  n = int(input())
-  
-#call your square() function here
-# write square(n)
+
