51061ff5a6f40cbb3bcd17bf78cdb66db8e2d64f
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 11:06:06 +0530

    Update main.py

--- P4/T6/main.py
+++ P4/T6/main.py
@@ -1,10 +1,12 @@
+# Write your code inside solution function given below 
+# values to integer name x and y is already given, you don't 
+# need to assign any values to them
+
+def solution(x, y):
+  #Write your code here
+ 
 # Test code
 # Don't change anything in this block
-  x = int(input())
-  y = int(input())
-  
- 
-# Write your code here 
-# values to variable x and y, you don't 
-# need to assign any values to them
- 
+x1 = int(input())
+y1 = int(input())
+solution(x1, y1)
