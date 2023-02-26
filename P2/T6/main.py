4ee1a3e25c2fba0c1047ae88e888d4a36c3dd5f2
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 11:02:37 +0530

    Update main.py

--- P2/T6/main.py
+++ P2/T6/main.py
@@ -1,14 +1,11 @@
+# Write your code inside solution function given below 
+# values to integer name n is already given, you don't 
+# need to assign any values to them
+
+def solution(n):
+  #Write your code here
+ 
 # Test code
 # Don't change anything in this block
 n = int(input())
-  
-# Write your code here 
-# Remember the output should match exactly
-# Here testing system will provide
-# values to variable n, you don't 
-# need to assign any values to them
-# Complete the sample code provided below
-    
-for i in range(1, n+1):
-  print()           # print pattern here in format given
-  
+solution(n)
