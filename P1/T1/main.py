a2d417482a56db0aa3c74246cfb317823a09c2a5
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 09:50:17 +0530

    Update main.py

--- P1/T1/main.py
+++ P1/T1/main.py
@@ -1,6 +1,5 @@
 # Write your code inside solution function given below 
-# you don't 
-# need to assign any values to them
+# you don't need to assign any values to them
 
 def solution():
   # write your code here
