b44b4ba7c9005f44a76c405d0be013a13fce6ed4
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-02-26 10:16:38 +0530

    Update main.py

--- P2/T3/main.py
+++ P2/T3/main.py
@@ -1,13 +1,11 @@
+# Write your code inside solution function given below 
+# values to integer name n is already given, you don't 
+# need to assign any values to them
+
+def solution(n):
+  # write your code here
+ 
 # Test code
 # Don't change anything in this block
-  n = int(input())
-
-# Remember the output should match exactly
-# Misplaced character, extra spaces, 
-# missing character, letters not in exact case will
-# will fail the test  
-
-# no need to assign value of n
-# Write your code here
-
- 
+n = int(input())
+solution(n)
