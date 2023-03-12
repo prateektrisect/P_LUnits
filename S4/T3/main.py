9e9774fc405725a9225e26156060b80a70b3c895
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-12 16:38:37 +0530

    Update main.py

--- S4/T3/main.py
+++ S4/T3/main.py
@@ -1,1 +1,12 @@
+# write you function here
 
+# Test code
+# Don't change anything in this block
+lst1 = []  
+n = int(input())
+lst1 = [int(item) for item in input().split()]
+  
+  
+  
+# Write code here 
+# and call your function 
