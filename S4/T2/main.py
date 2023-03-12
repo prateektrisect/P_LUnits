7f39e04ac1d57266893e1f67083203b36fc8b414
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-12 16:35:35 +0530

    Update main.py

--- S4/T2/main.py
+++ S4/T2/main.py
@@ -7,14 +7,11 @@ def MultipleOf3(ele):
 
 # Test code
 # Don't change anything in this block
-if __name__ == "__main__":
-  # For list of integers
-  lst1 = []  
-  n = int(input())
-  lst1 = [int(item) for item in input().split()]
+lst1 = []  
+n = int(input())
+lst1 = [int(item) for item in input().split()]
   
   
   
-# Write code here 
-# and call your function 
+# Write code here and call your function 
 # passing elements of the list lst1 as input to the function
