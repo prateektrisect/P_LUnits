f39515b27d4222a812237b4e03c9d728947b6fe2
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-12 16:31:59 +0530

    Update main.py

--- S2/T2/main.py
+++ S2/T2/main.py
@@ -6,11 +6,9 @@
 
 # Test code
 # Don't change anything in this block
-if __name__ == "__main__":
-  # For list of integers
-  lst1 = []  
-  n = int(input())
-  lst1 = [int(item) for item in input().split()]
+lst1 = []  
+n = int(input())
+lst1 = [int(item) for item in input().split()]
   
   
   
