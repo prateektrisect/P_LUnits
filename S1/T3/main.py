7aa1ec61f1617a1fdc17755416e7517b8089cc2d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-12 14:12:22 +0530

    Update main.py

--- S1/T3/main.py
+++ S1/T3/main.py
@@ -1,12 +1,16 @@
-# Write code here
-# given list nmae is lst1
-lst1 = [1, 7, 9, 12]
-
-
-
 
 # Test code
 # Don't change anything in this block
 def square(n):
   squareN = n*n
   print(squareN)
+  
+  
+# Given list nmae is lst1
+lst1 = [1, 7, 9, 12]
+
+# Write code here
+
+
+
+
