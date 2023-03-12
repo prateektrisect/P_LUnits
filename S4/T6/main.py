7aaa5eb458acf18dccdeefab1746d41bee73c09d
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-12 16:40:31 +0530

    Update main.py

--- S4/T6/main.py
+++ S4/T6/main.py
@@ -1,9 +1,8 @@
 # Write code here to create the function
 
+
 # Test code
 # Don't change anything in this block
-if __name__ == "__main__":
- st = input()
+n = input()
   
 # Write code here to Call the function
-
