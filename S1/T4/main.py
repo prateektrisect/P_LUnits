db46fce0e950e59749d9a6c96ff73f5b8aa44839
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-14 12:37:09 +0530

    Update main.py

--- S1/T4/main.py
+++ S1/T4/main.py
@@ -1,18 +1,16 @@
-# Test code
-# Don't change anything in this block
-n = int(input())
 # Given square function
 def square(n):
   squareN = n*n
   print(square)
 
   
-
-
 # Write your code here
 # Create cube function 
 # you don't need to assign any value to n
 
 
 
- 
+# Test code
+# Don't change anything in this block
+n = int(input())
+
