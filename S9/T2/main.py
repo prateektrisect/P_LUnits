a13581d6d56bef236396c4c50d4abd2afa212d3b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-03-20 12:16:33 +0530

    Rename main.py to S9/T2/main.py

---
+++ S9/T2/main.py
@@ -0,0 +1,17 @@
+# Write your code inside solution function given below 
+# values to list name in_list is already given, you don't 
+# need to assign any values to them
+
+def solution(in_list):
+  # Write your code
+ 
+ 
+  
+  
+  
+
+# Test code
+# Don't change anything in this block
+n = int(input())
+in_list = [int(item) for item in input().split()]
+solution(in_list)
