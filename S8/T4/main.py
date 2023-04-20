b42186632c116b7c05f6c32d375ec53fb67fa4f9
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-20 11:30:21 +0530

    Update and rename Main.java to main.py

---
+++ S8/T4/main.py
@@ -0,0 +1,12 @@
+def solution(str1, str2):
+    # Write your code here
+    # Remember the output should match exactly
+  
+
+
+# Test code
+# Don't change anything below this line
+
+str1 = input()
+str2 = input()
+solution(str1, str2)
