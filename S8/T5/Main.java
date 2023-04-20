a57dff94c87dc5a3271bbf85f32d4c55e7e36b2b
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-20 11:31:40 +0530

    Update Main.java

--- S8/T5/Main.java
+++ S8/T5/Main.java
@@ -1,30 +1,9 @@
-import java.util.*;
-import java.util.Scanner;
-public class Main{
-
-  void solution(String[] array) {
-      
-    // Write code here
+def solution(in_list):
+    # Write code here
    
     
-//     print new array here
-        System.out.println(Arrays.toString()) // type name of new array inside Arrays.toString()
-   
-  }
 
-  //Test code
-  //Don't change anything below this line
-  public static void main(String[] args){
-    Main test = new Main();
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextLine();
-    String[] array = new String[n]; 
-    Scanner sc1 = new Scanner(System.in);
-    for(int i=0; i<n; i++)  
-    {  
-         
-        array[i]=sc1.nextLine();  
-    }  
-    test.solution(array);
-  }
-}
+# Test code
+# Don't change anything below this line
+    solution()
+  
