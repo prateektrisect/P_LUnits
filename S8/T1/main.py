c0a09529e8d704d8629dcddddbaf9aef0b4ba9c7
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-20 11:23:35 +0530

    Update and rename Main.java to main.py

---
+++ S8/T1/main.py
@@ -0,0 +1,15 @@
+def solution(n, x):
+    // Write your code here
+    // Remember the output should match exactly
+
+ 
+
+//Test code
+//Don't change anything below this line
+  
+ n = int(input())
+ x= int(input())
+   
+ solution(n, x)
+ 
+
