4cb784e70c8b5da89687a776e008f747c4290ce4
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-20 11:25:58 +0530

    Update and rename Main.java to main.py

---
+++ S8/T2/main.py
@@ -0,0 +1,19 @@
+def solution(my_list1):
+      # Write code here
+   
+    
+ 
+
+# Test code
+# Don't change anything below this line
+
+n = int(input())
+my_list1 = [] 
+    for(int i=0; i<n; i++)  
+    {  
+         
+        array[i]=sc.nextInt();  
+    }  
+    test.solution(array);
+  }
+}
