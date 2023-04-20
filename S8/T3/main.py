52c30bc877422aa50cf6111e9e418b6904127d39
Aartiarora22 <121791845+Aartiarora22@users.noreply.github.com>
2023-04-20 11:28:22 +0530

    Update and rename Main.java to main.py

---
+++ S8/T3/main.py
@@ -0,0 +1,11 @@
+def solution(in_list):
+    # Write code here
+   
+    
+
+
+# Test code
+# Don't change anything below this line
+n = int(input())
+in_list = [int(item) for item in input().split()]
+solution(in_list)
